f53185681f1e0c46ff86c3e3fbffb6c397a7b661
Neplaz Dimitri <neplaz.dimitri@gmail.com>
2020-08-04 09:29:04 +0200

    add step

--- features/step_definitions/login_step.rb
+++ features/step_definitions/login_step.rb
@@ -5,7 +5,22 @@ When /^I fill my email and password"$/ do |field, string|
     fill_in("Mot de passe*", with: "testingpassword")
 end
 
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, string|
+    fill_in(field, with: string) 
+end
 
 When /^I click "([^"]*)"$/ do |input|
     click_button(input) 
+end
+
+Then /^I should see "([^"]*)"$/ do |text|
+    within('div.base-form-error') do
+        expect(page).to have_content(text)
+    end
+end
+
+Then /^I should see in form error "([^"]*)"$/ do |text|
+    within('div.form-error-message') do
+        expect(page).to have_content(text)
+    end
 end
