7a4a607ad5519c04ca9a365c04ed82dcc33fded6
Neplaz Dimitri <neplaz.dimitri@gmail.com>
2020-08-04 13:12:01 +0200

    fix lang error

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,11 +15,9 @@ Dotenv.load
   Capybara.register_driver :headless_chrome do |app|
     caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: 'ALL' })
     opts = Selenium::WebDriver::Chrome::Options.new
-    profile = Selenium::WebDriver::Chrome::Profile.new
-    profile['intl.accept_languages'] = 'fr'
-    chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222]
+    chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222 --lang=fr-FR]
     chrome_args.each { |arg| opts.add_argument(arg) }
-    Capybara::Selenium::Driver.new(app, browser: :chrome, profile: profile, options: opts, desired_capabilities: caps)
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: opts, desired_capabilities: caps)
   end
 
   #Switch between :chrome / :headless_chrome to see tests run in chrome
@@ -29,5 +27,3 @@ Dotenv.load
   else
     Capybara.default_driver = :chrome
   end
-
-  Capybara.default_max_wait_time=60
