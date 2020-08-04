a72c12b9e62c5b78a90c544e497482db2e5047bd
Neplaz Dimitri <neplaz.dimitri@gmail.com>
2020-08-04 12:29:51 +0200

    add conf for chrome headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,31 @@
 require 'capybara/cucumber'
 require 'webdrivers'
 require 'selenium-webdriver'
+require 'dotenv'
+Dotenv.load
 
+#Chrome non-headless driver
+  Capybara.register_driver :chrome do |app|
+    profile = Selenium::WebDriver::Chrome::Profile.new
+    profile['intl.accept_languages'] = 'fr'
+    Capybara::Selenium::Driver.new(app, browser: :chrome, profile: profile)
+  end
 
-Capybara.default_driver = :selenium_chrome_headless
-Capybara.app_host = "https://app.livestorm.co/"
+#Chrome headless driver
+  Capybara.register_driver :headless_chrome do |app|
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: 'ALL' })
+    opts = Selenium::WebDriver::Chrome::Options.new
+    profile = Selenium::WebDriver::Chrome::Profile.new
+    profile['intl.accept_languages'] = 'fr'
+    chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222]
+    chrome_args.each { |arg| opts.add_argument(arg) }
+    Capybara::Selenium::Driver.new(app, browser: :chrome, profile: profile, options: opts, desired_capabilities: caps)
+  end
+
+  #Switch between :chrome / :headless_chrome to see tests run in chrome
+  case ENV['HEADLESS']
+  when 'true', 1, nil
+    Capybara.default_driver = :headless_chrome
+  else
+    Capybara.default_driver = :chrome
+  end
